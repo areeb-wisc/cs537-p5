aeb7608dcb04efb680015f66fde3d619bd4ac960
Mohammad Areeb <areeb@rockhopper-09.cs.wisc.edu>
2024-11-14 12:49:42 -0600

    syscalltest

--- xv6-public/syscalltest.c
+++ xv6-public/syscalltest.c
@@ -1,5 +1,13 @@
-#include <user.h>
-#include <stdio.h>
-int main() {
+#include "types.h"
+#include "user.h"
+// #include "wmap.h"
+
+int main(int argc, char* argv[]) {
+    // wmap(0,0,0,0);
+    // wunmap(0);
+    // struct wmapinfo* p = (struct wmapinfo*)malloc(sizeof(struct wmapinfo));
+    // getwmapinfo(p);
+    // va2pa(0);
+    // printf(1, "hello\n");
     return 0;
 }
